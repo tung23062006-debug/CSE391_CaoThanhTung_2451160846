a1d61d59e176b5ca543d22a2ed5553a524f6a9a5
TungCao <tung23062006@gmail.com>
2026-05-26 22:05:51 +0700

    PBT_07: Update phần B ( bài B1 commit trước nhầm thành B2 )

--- PBT_07/calculator.js
+++ PBT_07/calculator.js
@@ -33,4 +33,4 @@ console.log(calculate(10, "+", 5));    // 15
 console.log(calculate(10, "/", 0));    // Lỗi: Không thể chia cho 0
 console.log(calculate(10, "^", 5));    // Lỗi: Operator '^' không hợp lệ
 console.log(calculate("abc", "+", 5)); // Lỗi: Input không phải số
-console.log(calculate(2, "**", 10));   // 1024
+console.log(calculate(2, "**", 10));   // 1024 
